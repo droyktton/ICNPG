2973cfa04d5cf1bfb4a9257c290cb91a2b8c731f
AleK <droyktton@gmail.com>
2023-04-27 16:01:27 -0300

    retoques

--- Clases/Clases_Thrust/clase_04/simple/reduce.cu
+++ Clases/Clases_Thrust/clase_04/simple/reduce.cu
@@ -2,18 +2,35 @@
 #include<thrust/device_vector.h>
 #include<thrust/host_vector.h>
 #include<cstdio>
+#include "cpu_timer.h"
+#include <omp.h>
 
-int main()
+using namespace thrust::placeholders;
+
+int main(int argc, char **argv)
 {
-	thrust::host_vector<float> hx( 1000000 );
+	thrust::host_vector<float> hx( 10000000 );
 
 	thrust::generate(hx.begin(),hx.end(),rand);
+	thrust::transform(hx.begin(),hx.end(),hx.begin(),_1/RAND_MAX);
 
 	thrust::device_vector<float> dx=hx;
 
+	cpu_timer reloj;
+
+	reloj.tic();
 	float sum=thrust::reduce(dx.begin(),dx.end());
 
-	printf("suma=%f\n",sum);
+	//esta instruccion solo la necesito y es valida en cuda
+	#ifdef CUDA
+	cudaDeviceSynchronize();
+	#endif
+
+	printf("ejecutable:%s suma=%f en %lf ms\n", argv[0], sum, reloj.tac());
+
+	#ifdef OMP
+	printf("omp threads=%d",omp_get_num_threads());
+	#endif
 
 	return 0;
 }
