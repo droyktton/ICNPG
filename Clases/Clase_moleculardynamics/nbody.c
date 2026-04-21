f3b27eb50977c3a82812b69628b9f06314761e13
AleK <droyktton@gmail.com>
2026-04-21 14:58:11 -0300

    nbody serial

--- Clases/Clase_moleculardynamics/nbody.c
+++ Clases/Clase_moleculardynamics/nbody.c
@@ -72,7 +72,7 @@ int main(const int argc, const char** argv) {
   double avgTime = totalTime / (double)(nIters-1); 
 
 #ifdef SHMOO
-  printf("%d, %0.3f\n", nBodies, 1e-9 * nBodies * nBodies / avgTime);
+  printf("%d Bodies, average %0.3f Billion Interactions / second\n", nBodies, 1e-9 * nBodies * nBodies / avgTime);
 #else
   //printf("Average rate for iterations 2 through %d: %.3f +- %.3f steps per second.\n",
   //       nIters, rate);
